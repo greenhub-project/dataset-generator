b1da8a3c72395bafddad0aeff5036eaa6c47f92e
Hugo Matalonga <hmatalonga@gmail.com>
2018-06-08 21:59:53 +0100

    refactor: append contents file to csv file

--- src/utils.sh
+++ src/utils.sh
@@ -12,31 +12,34 @@ function unset_vars {
 
 # Routine to query a table into a csv file
 function run_query {
-  # Set csv file path
-  CSV_FILE="$TMP_DIR/$TABLE_NAME.csv"
+  # Set files path
+  TXT_FILE="$TMP_DIR/$TABLE_NAME.txt"
+  CSV_FILE="$WORK_DIR/$TABLE_NAME.csv"
   
   # Check if old temp file exists, then remove it
-  if [ -f $CSV_FILE ]; then
-    echo $USER_PASS | sudo -S rm $CSV_FILE
+  if [ -f $TXT_FILE ]; then
+    echo $USER_PASS | sudo -S rm $TXT_FILE
   fi
 
   # Query table column names
   QUERY="SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='$DB_NAME' AND TABLE_NAME='$TABLE_NAME';"
-  mysql -B --disable-column-names -u$DB_USER -p$DB_PASS $DB_NAME -e $QUERY > "$WORK_DIR/$TABLE_NAME.info"
+  mysql -B --disable-column-names -u$DB_USER -p$DB_PASS $DB_NAME -e $QUERY > $CSV_FILE
 
   # Transform newline to comma separated values
-  echo -e $(sed -n '1h;2,$H;${g;s/\n/,/g;p}' "$WORK_DIR/$TABLE_NAME.info") > "$WORK_DIR/$TABLE_NAME.info"
+  echo -e $(sed -n '1h;2,$H;${g;s/\n/,/g;p}' $CSV_FILE) > $CSV_FILE
 
-  # Query table into csv file
-  QUERY="SELECT * FROM $TABLE_NAME INTO OUTFILE '$CSV_FILE' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
+  # Query table into txt file
+  QUERY="SELECT * FROM $TABLE_NAME INTO OUTFILE '$TXT_FILE' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
   mysql -u$DB_USER -p$DB_PASS $DB_NAME -e $QUERY
 
-  # Change ownership of csv file
-  echo $USER_PASS | sudo -S chown $USER:$USER $CSV_FILE
+  # Change ownership of txt file
+  echo $USER_PASS | sudo -S chown $USER:$USER $TXT_FILE
 
-  # Move csv file to working directory
-  mv $CSV_FILE $WORK_DIR
+  # Move txt file to working directory
+  mv $TXT_FILE $WORK_DIR
 
-  # Remove header file
-  rm "$WORK_DIR/$TABLE_NAME.info"
+  cat $TXT_FILE >> $CSV_FILE 2>&1
+
+  # Remove txt file
+  rm $TXT_FILE
 }
