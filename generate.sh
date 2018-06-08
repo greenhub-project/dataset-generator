b77f6ef8c3b77bfcdb5515a2f46af282161d28e7
Hugo Matalonga <hmatalonga@gmail.com>
2018-06-08 21:49:56 +0100

    feat: add run query function

--- generate.sh
+++ generate.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-if [ ! -f .env ]; then
+if [ ! -f ".env" ]; then
   echo ".env file not found!"
   bash ./src/init.sh
   exit 0
 fi
 
 . ./src/utils.sh
+
+# Devices
+TABLE_NAME="devices"
+run_query
+
+echo "Creating zip file..."
+
+zip -r dataset.zip ~/dataset/*.csv
+
+mv ~/dataset/dataset.zip $PUBLIC_PATH
+
+echo "Cleaning temporary files..."
+rm ~/dataset/*.csv
 
 
--- src/utils.sh
+++ src/utils.sh
@@ -9,3 +9,34 @@ function export_vars {
 function unset_vars {
   unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
 }
+
+# Routine to query a table into a csv file
+function run_query {
+  # Set csv file path
+  CSV_FILE="$TMP_DIR/$TABLE_NAME.csv"
+  
+  # Check if old temp file exists, then remove it
+  if [ -f $CSV_FILE ]; then
+    echo $USER_PASS | sudo -S rm $CSV_FILE
+  fi
+
+  # Query table column names
+  QUERY="SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='$DB_NAME' AND TABLE_NAME='$TABLE_NAME';"
+  mysql -B --disable-column-names -u$DB_USER -p$DB_PASS $DB_NAME -e $QUERY > "$WORK_DIR/$TABLE_NAME.info"
+
+  # Transform newline to comma separated values
+  echo -e $(sed -n '1h;2,$H;${g;s/\n/,/g;p}' "$WORK_DIR/$TABLE_NAME.info") > "$WORK_DIR/$TABLE_NAME.info"
+
+  # Query table into csv file
+  QUERY="SELECT * FROM $TABLE_NAME INTO OUTFILE '$CSV_FILE' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
+  mysql -u$DB_USER -p$DB_PASS $DB_NAME -e $QUERY
+
+  # Change ownership of csv file
+  echo $USER_PASS | sudo -S chown $USER:$USER $CSV_FILE
+
+  # Move csv file to working directory
+  mv $CSV_FILE $WORK_DIR
+
+  # Remove header file
+  rm "$WORK_DIR/$TABLE_NAME.info"
+}
